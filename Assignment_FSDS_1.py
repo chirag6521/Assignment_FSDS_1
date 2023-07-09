101bb7c37e8f5b47d25fa1312703f762c7e6c2f6
chirag6521 <129366494+chirag6521@users.noreply.github.com>
2023-07-10 01:50:33 +0530

    Add files via upload

---
+++ Assignment_FSDS_1.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#Ans.1
+## Values 
+### values can be integer or string
+
+a = "Hello"
+print(a)
+print(type(a))
+
+
+# In[7]:
+
+
+b = -87.8
+print(type(b))
+
+
+# In[8]:
+
+
+c = 6
+print(type(c))
+
+
+# In[ ]:
+
+
+#Ans.2
+## string are the type of data we do store in a variable and variable is a place where information is stored in form of data.
+
+
+# In[13]:
+
+
+#Ans.3
+## Three different data types.
+### Integer , Float , String are the three types of data types.
+
+a=1
+print(type(a))
+
+b=2.0
+print(type(b))
+
+c="iNeuron"
+print(type(c))
+
+
+# In[15]:
+
+
+#Ans.4
+## Expression are combination of operands and operator.
+a = 1
+b = 2 
+c = a+b
+print(c)
+
+### Expression represents value any string is also an expressions since it represents the value of the string
+
+
+# In[ ]:
+
+
+#Ans.5
+## Expression is a word or symbolthat is value. statements can only be declared ,
+### Statements create side effects to be useful while expressions are values or execute to values.
+
+
+# In[17]:
+
+
+#Ans.6
+bacon = 22
+bacon + 1
+
+
+# In[21]:
+
+
+#Ans.7
+
+a = "spam" + "spamspam"
+print(a)
+
+b = "spam" * 3
+print(b)
+
+
+# In[ ]:
+
+
+#Ans.8
+## Because variable can't be no.
+
+
+# In[27]:
+
+
+#Ans.9
+## Int(),float(),Str()
+
+a=int(10)
+print(type(a))
+
+b=float(1.4)
+print(type(b))
+
+c=str("hello")
+print(type(c))
+
+
+# In[28]:
+
+
+#Ans.10
+
+Person = 'i have eaten' + 99 + 'burritos'
+print(Person)
+
+# As u can see 99 is an integer so it has to be string to add witth string.
+
+
+# In[ ]:
+
+
+
+
