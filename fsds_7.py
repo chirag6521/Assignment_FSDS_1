63ab6966af2d502aeb1492dd6bec80a667a7b758
chirag6521 <129366494+chirag6521@users.noreply.github.com>
2023-07-14 11:40:58 +0530

    Add files via upload

---
+++ fsds_7.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Ans.1
+
+first_variable = 10
+second_variable = 5
+print(f"First variable is {first_variable} & second variable is {second_variable}.")
+print(f"Addition: {first_variable} + {second_variable} = {addition}")
+print(f"Subtraction: {first_variable} - {second_variable} = {subtraction}")
+print(f"Multiplication: {first_variable} * {second_variable} = {multiplication}")
+print(f"Division: {first_variable} / {second_variable} = {division}")
+
+
+# In[6]:
+
+
+#Ans.2
+a=5
+b=10
+
+a1=a/b   ## This is called floating point evision.
+b1=a//b  ## This is floor division.
+
+print(a1)
+print(b1)
+
+
+# In[10]:
+
+
+#Ans.3
+## Some logical operator are or ,and,not,false,true.
+
+a=11
+b=22
+c=33
+
+print(a>b and b>c)
+
+print(a>b or b>c)
+
+print(not b>c)
+
+
+# In[13]:
+
+
+#Ans.4
+
+a=5
+a1=a>>2
+print(a1)  ## Right shift operator.
+
+b=5
+b1=a<<2
+print(b1)  ## Left shift operator.
+
+
+# In[16]:
+
+
+#Ans.5
+
+a=[1,2,3,45,6,7,8,9,0,8,10,2,34,4]
+
+if 10 in a:
+    print("10 Is the present in the list")
+else:
+    print("10 is not present in the list")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
